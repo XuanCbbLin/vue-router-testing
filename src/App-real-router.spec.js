da0be20c7fa0e7f77e077083a17867d46d357f67
Xuan <xuancbblin@gmail.com>
2023-06-07 11:28:00 +0800

    test: real router change

---
+++ src/App-real-router.spec.js
@@ -0,0 +1,39 @@
+import App from '@/App.vue'
+import { vi, expect, describe, it, beforeEach } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from '@/router'
+
+// Using a real router with Composition API(https://test-utils.vuejs.org/guide/advanced/vue-router.html#using-a-real-router-with-composition-api)
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+describe('change page', () => {
+  beforeEach(async () => {
+    // navigate to the home page
+    router.push('/')
+    await router.isReady()
+  })
+
+  it('should switch to /about', async () => {
+    // Arrange
+    const wrapper = mount(App, {
+      global: {
+        // install vue-router
+        plugins: [router]
+      }
+    })
+
+    // Act
+    // spy on router.push
+    const push = vi.spyOn(router, 'push')
+
+    await wrapper.find('[data-test="about"]').trigger('click')
+
+    // Assert
+    expect(push).toHaveBeenCalledTimes(1)
+    expect(push).toHaveBeenCalledWith('/about')
+  })
+})
